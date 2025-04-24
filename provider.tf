51ab46fe74f3e39f1501f18e723d2c0f77f57058
Josh <josh21.09.96@live.de>
2025-04-24 22:06:06 +0200

    new plugin

--- provider.tf
+++ provider.tf
@@ -1,31 +1,16 @@
 terraform {
-
-        required_providers {
-                proxmox = {
-                        source = "telmate/proxmox"
-                        version = "2.9.14"
-                }
-        }
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
 }
 
-variable "proxmox_api_url" {
-        type = string
-}
-
-variable "proxmox_api_token_id" {
-        type = string
-        sensitive = true
-}
-
-variable "proxmox_api_token_secret" {
-        type =  string
-        sensitive = true
-}
-
-provider "proxmox" { 
-
-        pm_api_url= var.proxmox_api_url
-        pm_api_token_id = var.proxmox_api_token_id
-        pm_api_token_secret = var.proxmox_api_token_secret
-        pm_tls_insecure = true
+provider "proxmox" {
+  endpoint  = var.virtual_environment_endpoint
+  api_token = var.virtual_environment_token
+  insecure = true
+  ssh {
+    agent = true
+  }
 }
